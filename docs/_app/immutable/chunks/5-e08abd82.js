9f9263f2dd6f456c331df1ae7f06358d76d24f78
Annie Eliasson <anneli03@edu.umea.se>
2024-02-19 12:00:56 +0100

    starttt klarttt

---
+++ docs/_app/immutable/chunks/5-e08abd82.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-0952c892.js";export{t as component};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,5 +5,14 @@
 
 <h2>Här har jag samlat min uppgifter jag gjort i samband med webbutveckling i årskurs 2 på Maja Beskowgymnasiet!</h2>
 
-<h3 >Förnamn Efternamn</h3>
+<h3
+class="name"> Annie Eliasson
+</h3>
+
+<style>
+h1{
+color: rebeccapurple;
+font-family:'Gill Sans'
+}
+</style>
 
