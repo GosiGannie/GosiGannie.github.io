5b7938fb8fed1eed36456c0c948a89b93b2f3955
Annie Eliasson <anneli03@edu.umea.se>
2024-02-26 12:01:28 +0100

    klar med photosssssssss

---
+++ src/routes/photoapp/+page.svelte
@@ -0,0 +1,31 @@
+<title> Photo App </title> 
+
+<h1>Photo App</h1>
+
+<div class = "photos">
+    {#each images as img}
+        <div class = 'image-wrapper'>
+            <img src = {img} alt = 'flexbox rules'>
+        </div>
+        {/each}
+</div>
+
+<style>
+    .photos{
+        display:flex;
+        max-width: 90vw;
+    }
+
+    .photos img {
+        max-width: 90%;
+        align-items: center;
+        justify-content: space-around;
+    }
+</style>
+
+<script>
+    let image = 'https://picsum.photos/id/70/367/267'
+    let photo = 'https://picsum.photos/id/88/367/267'
+    let picture = 'https://picsum.photos/id/93/367/267'
+    let images = [image, photo, picture, image, photo, picture]
+</script>
