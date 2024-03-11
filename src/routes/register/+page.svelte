68465f249009965a0bcb7190639484caae03a312
Annie Eliasson <anneli03@edu.umea.se>
2024-03-11 12:11:57 +0100

    klar med registerrrrr

---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,34 @@
+<h1>Registration Form</h1>
+
+<form on:submit|preventDefault={handleSubmit}>
+    <label for='firstName'>First Name:</label>
+    <input id='firstName' type='text'/>
+    <label for='lastName'>Last Name:</label>
+    <input id='lastName' type='text'/>
+    <label for='email'>Email:</label>
+    <input id='email' type='email'/>
+    <label for='password'>Password:</label>
+    <input id='password' type='password'/>
+
+    <button type='submit'>Register</button>
+ </form>
+
+ <script>
+    function handleSubmit() {
+        let name = 'Form Submitted!';
+        console.log(name);
+    }
+</script>
+
+<style>
+    form{display: grid;
+    width: 60%;
+    max-width: 500;
+    min-width: 300;
+    margin: auto;
+    grid-template-columns: repeat(2, minmax(200px, 1fr));
+    }
+    :global(body){
+        background-color: #EEC5DF;
+    }
+</style>
