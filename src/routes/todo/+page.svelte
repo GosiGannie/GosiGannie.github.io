c2de37183abab49c57939ac4d79d0b4ff72c6df9
Annie Eliasson <anneli03@edu.umea.se>
2024-03-18 11:59:52 +0100

    Inte klar med todo

---
+++ src/routes/todo/+page.svelte
@@ -0,0 +1,35 @@
+<div class='container'>
+    <h1>TODO LIST</h1>
+        <form on:submit|preventDefault={handleSubmit}>
+            <label for='input'>TODO:</label>
+            <input id='input' type='text'/>
+        </form>
+    <div class='layout'>
+        <div style='display:grid'>
+            <h1>NEW</h1>
+        </div>
+        <div style='display:grid'>
+            <h1>COMPLETED</h1>
+        </div>
+    </div>
+</div>
+
+<script>
+    function handleSubmit() {
+        let name = 'Form Submitted!';
+        console.log(name);
+    }
+</script>
+
+<style>
+    .container{
+        width: 80vw;
+        height: 70vh;
+        background-color: cornflowerblue;
+        border-width: 10px;
+        border-style: solid;
+        border-color: black;
+        border-radius: 3%;
+        padding-top: 10px;
+    }
+</style>
